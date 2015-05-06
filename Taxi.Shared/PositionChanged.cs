207855ab93cdaa26a9b5fc9dbf85793d35d23218
rogeralsing <rogeralsing@gmail.com>
2015-05-06 13:10:00 +0200

    foo

--- Taxi.Shared/PositionChanged.cs
+++ Taxi.Shared/PositionChanged.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Taxi.Shared
+{
+    class PositionChanged
+    {
+        private decimal p1;
+        private decimal p2;
+
+        public PositionChanged(decimal p1, decimal p2)
+        {
+            // TODO: Complete member initialization
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+    }
+}
