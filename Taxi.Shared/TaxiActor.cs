f930496fae0eb1a4c79754a851c9161626f7e70a
rogeralsing <rogeralsing@gmail.com>
2015-05-06 10:14:20 +0200

    initial commit

---
+++ Taxi.Shared/TaxiActor.cs
@@ -0,0 +1,53 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi.Shared
+{
+    public class TaxiActor : ReceiveActor
+    {
+        private readonly Queue<Position> _positions = new Queue<Position>(); 
+        private Position _position;
+        private bool _idle;
+        private ICancelable _idleTimer;
+
+        public TaxiActor ()
+        {
+
+            Receive<Idle>(_ =>
+            {
+                _idle = true;
+                //TODO: notify server?
+            });
+
+            Receive<GpsPosition>(p =>
+            {                
+                _position = new Position()
+                {
+                    //TODO: uppdatera
+                };
+                _positions.Enqueue(_position);
+                
+                if (_idleTimer != null)
+                    _idleTimer.Cancel();
+
+                _idleTimer = Context.System.Scheduler.ScheduleTellOnceCancelable(TimeSpan.FromMinutes(1),Self,new Idle(),Self);
+                _idle = false;
+            });
+        }
+    }
+
+    public class Idle
+    {
+        
+    }
+
+    public class Position
+    {
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+    }
+}
---
+++ TaxiHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace TaxiHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var system = ActorSystem.Create("TaxiHost"))
+            {                
+                Console.ReadLine();
+            }
+        }
+    }
+}
