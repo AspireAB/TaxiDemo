b320e3679e554ae365a791d2f9d0370d083350b7
rogeralsing <rogeralsing@gmail.com>
2015-05-10 21:56:32 +0200

    async await

--- WebApplication2/SignalRActor.cs
+++ WebApplication2/SignalRActor.cs
@@ -1,24 +1,31 @@
 ﻿using Akka.Actor;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
 using TaxiShared;
 using WebApplication2.Hubs;
 
 
 namespace WebApplication2
 {
-   public class SignalRActor : ReceiveActor
-   {
-      public SignalRActor()
-      {
-         var chat = GlobalHost.ConnectionManager.GetHubContext<PositionHub>();
-         Receive<Taxi.PositionBearing>(p =>
-         {
-            chat.Clients.All.positionChanged(p);
-         });
-         Receive<Taxi.Status>(status =>
-         {
-            chat.Clients.All.statusChanged(status);
-         });
-      }
-   }
+    public class SignalRActor : ReceiveActor
+    {
+        private readonly IHubContext _chat;
+
+        public SignalRActor()
+        {
+            _chat = GlobalHost.ConnectionManager.GetHubContext<PositionHub>();
+            Receive<Taxi.PositionBearing>(p => positionChanged(p));
+            Receive<Taxi.Status>(status => statusChanged(status));
+        }
+
+        private async Task statusChanged(Taxi.Status status)
+        {
+            await _chat.Clients.All.statusChanged(status);
+        }
+
+        private async Task positionChanged(Taxi.PositionBearing p)
+        {
+            await _chat.Clients.All.positionChanged(p);
+        }
+    }
 }
